b5946559cf7fb3457275853bc1ff172051ba2f62
kagekyaa <kevin_hokagero@yahoo.com>
2015-11-21 22:18:37 -0800

    added

--- testSpeech2.py
+++ testSpeech2.py
@@ -1,16 +1,16 @@
 import pyoxford
 from tokens import *
 
-text = "a b c d e f"
+text = "star"
 api = pyoxford.speech("kage-test-speech", oxford_computer_speech)
 
 # text to speech (.wav file)
 binary = api.text_to_speech(text)
-with open("sound1.wav", "wb") as f:
+with open("voice.wav", "wb") as f:
     f.write(binary)
 
 # speech to text
-recognized = api.speech_to_text("sound1.wav")
+recognized = api.speech_to_text("voice.wav")
 print(recognized)
 
 if text == recognized:
